129a78ebd975bd9d0dc483746ff0cc436b8068ca
ddfeliz <sambatratahirindrazana@gmail.com>
2025-08-11 11:47:50 +0300

    MR 3

--- webapp/src/api/base_url.ts
+++ webapp/src/api/base_url.ts
@@ -1,4 +1,5 @@
-const BASE_URL = 'https://gestion-vacation.onrender.com';
+// const BASE_URL = 'https://gestion-vacation.onrender.com';
+const BASE_URL = 'https://mern-vacation.vercel.app';
 
 export default BASE_URL;
 export interface APIArchive {
